413712de82bfefe319c75b27212186e0d250fb4d
Shazman96 <admin@dcastack.com>
2022-07-09 15:54:20 -0400

    General housekeeping

--- contracts/DCAStack.sol
+++ contracts/DCAStack.sol
@@ -4,10 +4,8 @@ pragma solidity ^0.8.0;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-import "./UserTrade.sol";
-
 //create new user schedule
-contract DCAStack is Ownable, UserTrade {
+contract DCAStack is Ownable {
 
 
 }
